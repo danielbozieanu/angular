afb6b3c6d35cd8db9d19c18dc5f5a5aca63a66d2
DESKTOP-LDT60J1\LOW1 <office@landofweb.ro>
2016-12-22 16:24:05 +0200

    Tutorial nr 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
